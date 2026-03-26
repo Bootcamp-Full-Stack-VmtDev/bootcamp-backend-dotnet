553f117067d9e6cdd2c0db330b0298b1e316ebb7
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2026-03-26 09:43:20 -0500

    feat(interface): agregar interface del service para crear instructor

---
+++ laboratorio/LaboratorioUdemy/LaboratorioUdemy.Application/Interfaces/Services/IInstructorService.cs
@@ -0,0 +1,11 @@
+﻿using LaboratorioUdemy.Application.Models.DTOs;
+using LaboratorioUdemy.Application.Models.Requests.Instructor;
+using LaboratorioUdemy.Application.Models.Responses;
+
+namespace LaboratorioUdemy.Application.Interfaces.Services
+{
+    public interface IInstructorService
+    {
+        public GenericResponse<InstructorDto> Create(CreateInstructorRequest model);
+    }
+}
