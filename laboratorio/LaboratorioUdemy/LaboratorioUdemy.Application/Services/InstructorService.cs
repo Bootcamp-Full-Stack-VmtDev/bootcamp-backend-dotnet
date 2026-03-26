1dfc598e22942efa2f07ef8a398efba2c7a935a4
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2026-03-26 09:57:37 -0500

    feat(application): agregar helper para el response

---
+++ laboratorio/LaboratorioUdemy/LaboratorioUdemy.Application/Services/InstructorService.cs
@@ -0,0 +1,23 @@
+﻿using LaboratorioUdemy.Application.Interfaces.Services;
+using LaboratorioUdemy.Application.Models.DTOs;
+using LaboratorioUdemy.Application.Models.Requests.Instructor;
+using LaboratorioUdemy.Application.Models.Responses;
+using LaboratorioUdemy.Shared;
+using LaboratorioUdemy.Shared.Helpers;
+
+namespace LaboratorioUdemy.Application.Services
+{
+    public class InstructorService(Cache<InstructorDto> cache) : IInstructorService
+    {
+        public GenericResponse<InstructorDto> Create(CreateInstructorRequest model)
+        {
+            var instructor = new InstructorDto
+            {
+                InstructorId = Guid.NewGuid(),
+                Name = model.Name,
+                Email = model.Email,
+                RegisterDate = DateTimeHelper.UtcNow()
+            };
+        }
+    }
+}
