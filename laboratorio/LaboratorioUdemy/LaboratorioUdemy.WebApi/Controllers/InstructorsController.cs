119baeeef33fc34cfad52d60f388ea94e2c37056
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2026-03-26 09:06:26 -0500

    feat(controller): agregar controllador base para crear instructor

---
+++ laboratorio/LaboratorioUdemy/LaboratorioUdemy.WebApi/Controllers/InstructorsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LaboratorioUdemy.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstructorsController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return Ok("Colaborador creado correctamente!");
+        }
+    }
+}
