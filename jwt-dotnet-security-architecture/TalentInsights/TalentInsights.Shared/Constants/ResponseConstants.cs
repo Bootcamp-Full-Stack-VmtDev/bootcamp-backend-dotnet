4dbbd1a7aa0bea2ffdecdfe9f8c4e7fe41a017f8
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2026-04-15 14:55:06 -0500

    refactor(shared): update response and validation constants

--- jwt-dotnet-security-architecture/TalentInsights/TalentInsights.Shared/Constants/ResponseConstants.cs
+++ jwt-dotnet-security-architecture/TalentInsights/TalentInsights.Shared/Constants/ResponseConstants.cs
@@ -5,17 +5,26 @@ namespace TalentInsights.Shared.Constants
         // Collaborators
         public const string COLLABORATOR_NOT_EXISTS = "El colaborador no existe";
 
+        // Roles
+        public static string RoleNotFound(string name) => $"El rol {name} no existe";
+        public static string RoleNotFound(Guid id) => $"El rol con ID: {id} no existe";
+
         // Projects
         public const string PROJECT_NOT_EXISTS = "El proyecto no existe";
 
+        // Auth
+        public const string AUTH_TOKEN_NOT_FOUND = "El token no es correcto, expiró o no se argumentó";
+        public const string AUTH_USER_OR_PASSWORD_NOT_FOUND = "Usuario o contraseña incorrectos";
+        public const string AUTH_REFRESH_TOKEN_NOT_FOUND = "El token para refrescar la sesión expiró, no existe o es incorrecto";
+
         public static string ErrorUnexpected(string traceId)
         {
-            return $"Ha ocurrido un error inesperador: Contacto con soporte, mencionando el siguiente código de error: {traceId}";
+            return $"Ha ocurrido un error inesperado: Contacto con soporte, mencionando el siguiente código de error: {traceId}";
         }
 
         public static string ConfigurationPropertyNotFound(string property)
         {
-            return $"Falta la propiedad {property} por establecer en la configuracion de la aplicacion";
+            return $"Falta la propiedad '{property}' por establecer en la configuración del aplicativo.";
         }
     }
 }
